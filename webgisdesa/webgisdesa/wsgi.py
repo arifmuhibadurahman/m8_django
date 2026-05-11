bc8dd231a1d508c812411cf2ebca0b730dd992f1
saber roam <106901786+arifmuhibadurahman@users.noreply.github.com>
2026-05-11 23:23:24 +0700

    ubah wsgi error 500

--- webgisdesa/webgisdesa/wsgi.py
+++ webgisdesa/webgisdesa/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for webgisdesa project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Menghitung path ke folder yang berisi 'manage.py'
+# Agar Django bisa menemukan folder 'webgisdesa.settings'
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webgisdesa.settings")
 
 application = get_wsgi_application()
+
+# Baris ini sangat krusial untuk Vercel (Serverless Function)
+app = application
